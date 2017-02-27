256b11a7e8a8d9eed754c2697dc732879917b359
mochidamochiko <lotrftr.blackshadow@gmail.com>
2017-02-27 23:46:56 +0900

    Get billing data.

---
+++ get_current_billing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import boto3
+import pprint
+import datetime
+
+pp = pprint.PrettyPrinter(indent=4)
+
+cloudwatch = boto3.client('cloudwatch', region_name='us-east-1')
+
+# list_metrics = cloudwatch.list_metrics()
+response = cloudwatch.get_metric_statistics(
+    Namespace = 'AWS/Billing',
+    MetricName = 'EstimatedCharges',
+    Dimensions = [
+        {
+            "Name": "Currency",
+            "Value": "USD"
+        }
+    ],
+    StartTime = datetime.datetime.utcnow() - datetime.timedelta(seconds=3600),
+    EndTime = datetime.datetime.utcnow(),
+    Period = 3600,
+    Statistics = ['Maximum']
+)
+
+pp.pprint(response)
+
+# pp.pprint(list_metrics)
